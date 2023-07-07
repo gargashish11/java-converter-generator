a3224892ca81f6b8355de968c61e5372770c6d4a
Ashish Garg <gargashish11@gmail.com>
2023-07-07 08:33:03 +0530

    updates

--- src/main/java/com/ashish/converter/controllers/DefaultController.java
+++ src/main/java/com/ashish/converter/controllers/DefaultController.java
@@ -2,7 +2,6 @@ package com.ashish.converter.controllers;
 
 import com.ashish.converter.pojo.ConverterInputData;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.text.CaseUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,16 +27,35 @@ public class DefaultController {
         String source = converterInputData.getSource();
         String target = converterInputData.getTarget();
         String[] sourceTarget = {source + ".get", target + ".set"};
-
-        var output = Arrays.stream(converterInputData.getFields().split(","))
+        var output = Arrays.stream(converterInputData.getFields().split("((\\W))"))
                 .map(String::strip)
                 .filter(Predicate.not(StringUtils::isEmpty))
                 .filter(Predicate.not(StringUtils::containsWhitespace))
+                .filter(Predicate.not(s -> s.matches("private")))
+                .filter(Predicate.not(s -> s.matches("[A-Z]\\w*")))
                 .map(StringUtils::capitalize)
-                .map(s -> StringUtils.join(sourceTarget[1], s,"(",sourceTarget[0]+s+");"))
+                .map(s -> StringUtils.join(sourceTarget[1],
+                        s,
+                        "(",
+                        sourceTarget[0],
+                        s,
+                        "());"))
                 .map(s -> s.concat(newLine))
                 .toList();
-        converterInputData.setOutput(output.stream().collect(Collectors.joining()));
+
+        StringBuilder finalOutput = new StringBuilder(StringUtils.capitalize(target));
+        finalOutput.append(" ")
+                .append(target)
+                .append("=")
+                .append("new ")
+                .append(StringUtils.capitalize(target))
+                .append("();")
+                .append(newLine)
+                .append(output.stream().collect(Collectors.joining()))
+                .append("return ")
+                .append(target)
+                .append(";");
+        converterInputData.setOutput(finalOutput.toString());
         model.addAttribute("converterInputData", converterInputData);
         return "index";
     }
